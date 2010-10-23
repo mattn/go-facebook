5d0d9e4325dff1e91f7cce6d5a61657ad6aabb76
Marcel Hauf <marcel.hauf@googlemail.com>
2010-10-23 23:08:49 +0200

    Format with gofmt

--- src/graph/graph.go
+++ src/graph/graph.go
@@ -1,141 +1,151 @@
 package facebook
 
-import(
-  "http"
-  "io/ioutil"
-  "json"
-  "os"
-  "strconv"
+import (
+	"http"
+	"io/ioutil"
+	"json"
+	"os"
+	"strconv"
 )
 
 const (
-  GRAPHURL = "http://graph.facebook.com/"
+	GRAPHURL = "http://graph.facebook.com/"
 )
 
 type Args struct {
-  Metadata bool
+	Metadata bool
 }
 
 type Page struct {
-  ID string
-  Name string
-  Picture string
-  Link string
-  Category string
-  Website string
-  Username string
-  Founded string
-  CompanyOverview string
-  Mission string
-  Products string
-  FanCount float64
+	ID              string
+	Name            string
+	Picture         string
+	Link            string
+	Category        string
+	Website         string
+	Username        string
+	Founded         string
+	CompanyOverview string
+	Mission         string
+	Products        string
+	FanCount        float64
 }
 
 func (p *Page) String() string {
-  return "ID: " + p.ID + "\tName: " + p.Name + "\tPicture: " + p.Picture +
-	"\tLink: " + p.Link + "\tCategory: " + p.Category + "\tWebsite: " +
-	p.Website + "\tUsername: " + p.Username + "\tFounded: " + p.Founded +
-	"\tCompany overview: " + p.CompanyOverview + "\tMission: " + p.Mission +
-	"\tProducts: " + p.Products + "\tFan count:" +
-	strconv.Ftoa64(p.FanCount, 'e', -1) + "\n";
+	return "ID: " + p.ID + "\tName: " + p.Name + "\tPicture: " + p.Picture +
+		"\tLink: " + p.Link + "\tCategory: " + p.Category + "\tWebsite: " +
+		p.Website + "\tUsername: " + p.Username + "\tFounded: " + p.Founded +
+		"\tCompany overview: " + p.CompanyOverview + "\tMission: " + p.Mission +
+		"\tProducts: " + p.Products + "\tFan count:" +
+		strconv.Ftoa64(p.FanCount, 'e', -1) + "\n"
 }
 
 type Person struct {
-  ID string
-  Name string
-  FirstName string
-  LastName string
-  Link string
-  Gender string
-  Locale string
-  UpdatedTime string
+	ID          string
+	Name        string
+	FirstName   string
+	LastName    string
+	Link        string
+	Gender      string
+	Locale      string
+	UpdatedTime string
 }
 
 func (p *Person) String() string {
-  return "ID: " + p.ID + "\tName: " + p.Name + "\tFirst name: " + p.FirstName +
-	"\tLast name: " + p.LastName + "\tLink: " + p.Link + "\tGender: " +
-	p.Gender + "\tLocale: " + p.Locale + "\tUpdated time: " + p.UpdatedTime +
-	"\n";
+	return "ID: " + p.ID + "\tName: " + p.Name + "\tFirst name: " + p.FirstName +
+		"\tLast name: " + p.LastName + "\tLink: " + p.Link + "\tGender: " +
+		p.Gender + "\tLocale: " + p.Locale + "\tUpdated time: " + p.UpdatedTime +
+		"\n"
 }
 
 func FetchPage(id string) (page Page, err os.Error) {
-  body, err := fetchBody(id)
-  if err != nil { return }
-  data, err := getJsonMap(body)
-  if err != nil { return }  
-  for key, value := range data {
-	switch key {
-	  case "website":
-		page.Website = value.(string)
-	  case "picture":
-		page.Picture = value.(string)
-	  case "fan_count":
-		page.FanCount = value.(float64)
-	  case "mission":
-		page.Mission = value.(string)
-	  case "category":
-		page.Category = value.(string)
-	  case "name":
-		page.Name = value.(string)
-	  case "username":
-		page.Username = value.(string)
-	  case "link":
-		page.Link = value.(string)
-	  case "id":
-		page.ID = value.(string)
-	  case "products":
-		page.Products = value.(string)
-	  case "founded":
-		page.Founded = value.(string)
-	  case "company_overview":
-		page.CompanyOverview = value.(string)
+	body, err := fetchBody(id)
+	if err != nil {
+		return
 	}
-  }  
-  return
+	data, err := getJsonMap(body)
+	if err != nil {
+		return
+	}
+	for key, value := range data {
+		switch key {
+		case "website":
+			page.Website = value.(string)
+		case "picture":
+			page.Picture = value.(string)
+		case "fan_count":
+			page.FanCount = value.(float64)
+		case "mission":
+			page.Mission = value.(string)
+		case "category":
+			page.Category = value.(string)
+		case "name":
+			page.Name = value.(string)
+		case "username":
+			page.Username = value.(string)
+		case "link":
+			page.Link = value.(string)
+		case "id":
+			page.ID = value.(string)
+		case "products":
+			page.Products = value.(string)
+		case "founded":
+			page.Founded = value.(string)
+		case "company_overview":
+			page.CompanyOverview = value.(string)
+		}
+	}
+	return
 }
 
 func FetchPerson(name string) (person Person, err os.Error) {
-  body, err := fetchBody(name)
-  if err != nil { return }
-  data, err := getJsonMap(body)
-  if err != nil { return }
-  for key, value := range data {
-	switch key {
-	  case "locale":
-		person.Locale = value.(string)
-	  case "name":
-		person.Name = value.(string)
-	  case "link":
-		person.Link = value.(string)
-	  case "Gender":
-		person.Gender = value.(string)
-	  case "first_name":
-		person.FirstName = value.(string)
-	  case "last_name":
-		person.LastName = value.(string)
-	  case "id":
-		person.ID = value.(string)
+	body, err := fetchBody(name)
+	if err != nil {
+		return
 	}
-  }  
-  return
+	data, err := getJsonMap(body)
+	if err != nil {
+		return
+	}
+	for key, value := range data {
+		switch key {
+		case "locale":
+			person.Locale = value.(string)
+		case "name":
+			person.Name = value.(string)
+		case "link":
+			person.Link = value.(string)
+		case "Gender":
+			person.Gender = value.(string)
+		case "first_name":
+			person.FirstName = value.(string)
+		case "last_name":
+			person.LastName = value.(string)
+		case "id":
+			person.ID = value.(string)
+		}
+	}
+	return
 }
 
 
-func getJsonMap(body []byte) (data map[string] interface{}, err os.Error) {
-  var values interface{}
-  
-  if err = json.Unmarshal(body, &values); err != nil {
+func getJsonMap(body []byte) (data map[string]interface{}, err os.Error) {
+	var values interface{}
+
+	if err = json.Unmarshal(body, &values); err != nil {
+		return
+	}
+	data = values.(map[string]interface{})
 	return
-  }
-  data = values.(map[string] interface{})
-  return
 }
 
 func fetchBody(method string) (body []byte, err os.Error) {
-  resp, _, err := http.Get(GRAPHURL + method) // Response, final URL, error
-  if err != nil { return }
-  defer resp.Body.Close()
-  
-  body, err = ioutil.ReadAll(resp.Body)
-  return
+	resp, _, err := http.Get(GRAPHURL + method) // Response, final URL, error
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	return
 }
--- src/graph/graph_test.go
+++ src/graph/graph_test.go
@@ -1,40 +1,40 @@
 package facebook
 
-import(
-  "testing"
+import (
+	"testing"
 )
 
 // Tests
 
 func TestPage(t *testing.T) {
-  id := "19292868552"
-  t.Logf("Fetching facebook page %s\n", id)
-  _,err := FetchPage(id)
-  if err != nil {
-	t.Errorf("Error: %s\n", err.String())
-  }
+	id := "19292868552"
+	t.Logf("Fetching facebook page %s\n", id)
+	_, err := FetchPage(id)
+	if err != nil {
+		t.Errorf("Error: %s\n", err.String())
+	}
 }
 
 
 func TestPerson(t *testing.T) {
-  name := "btaylor"
-  t.Logf("Fetching facebook person %s\n", name)
-  _,err := FetchPerson(name)
-  if err != nil {
-	t.Errorf("Error: %s\n", err.String())
-  }
+	name := "btaylor"
+	t.Logf("Fetching facebook person %s\n", name)
+	_, err := FetchPerson(name)
+	if err != nil {
+		t.Errorf("Error: %s\n", err.String())
+	}
 }
 
 // Benchmarks
 
 func BenchmarkPage(b *testing.B) {
-  for i := 0; i < b.N; i++ {
-	FetchPerson("19292868552")
-  }
+	for i := 0; i < b.N; i++ {
+		FetchPerson("19292868552")
+	}
 }
 
 func BenchmarkPerson(b *testing.B) {
-  for i := 0; i < b.N; i++ {
-	FetchPerson("btaylor")
-  }
+	for i := 0; i < b.N; i++ {
+		FetchPerson("btaylor")
+	}
 }
