232fe519d2889fa13ab42b44b617530fac5e9937
Marcel Hauf <marcel.hauf@googlemail.com>
2010-10-31 21:47:24 +0100

    Extract Comment from home.go to comment.go

--- src/graph/home.go
+++ src/graph/home.go
@@ -81,25 +81,3 @@ func parseNews(value []interface{}) (news []News, err os.Error) {
 	}
 	return
 }
-
-type Comment struct {
-	ID          string
-	From        Object
-	Message     string
-	CreatedTime string
-}
-
-func parseComment(value map[string]interface{}) (comment Comment) {
-	comment.ID = value["id"].(string)
-	comment.From = parseObject(value["from"].(map[string]interface{}))
-	comment.Message = value["message"].(string)
-	comment.CreatedTime = value["created_time"].(string)
-	return
-}
-
-func parseComments(value []interface{}) (comments []Comment) {
-	for i, v := range value {
-		comments[i] = parseComment(v.(map[string]interface{}))
-	}
-	return
-}
