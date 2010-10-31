33936f1e1c37165560346acc51b0a83c6839b273
Marcel Hauf <marcel.hauf@googlemail.com>
2010-10-31 21:49:47 +0100

    Extract News from home.go to news.go

---
+++ src/graph/news.go
@@ -0,0 +1,53 @@
+package facebook
+
+import (
+	"os"
+	"time"
+)
+
+type News struct {
+	ID          string
+	From        Object
+	To          []Object
+	Message     string
+	Actions     []Link
+	Type        string
+	CreatedTime *time.Time
+	UpdatedTime *time.Time
+	Likes       string
+	Comments    []Comment
+}
+
+func parseNews(value []interface{}) (news []News, err os.Error) {
+	news = make([]News, len(value))
+	for i, v := range value {
+		wp := v.(map[string]interface{})
+		for key, val := range wp {
+			switch key {
+			case "id":
+				news[i].ID = val.(string)
+			case "from":
+				news[i].From = parseObject(val.(map[string]interface{}))
+			case "to":
+				data := val.(map[string]interface{})
+				news[i].To = parseObjects(data["data"].([]interface{}))
+			case "message":
+				news[i].Message = val.(string)
+			case "actions":
+				news[i].Actions = parseLinks(val.([]interface{}))
+			case "type":
+				news[i].Type = val.(string)
+			case "created_time":
+				news[i].CreatedTime, err = time.Parse("RFC3339", val.(string)) // Eg.: 2010-10-21T22:54:34+0000
+			case "updated_time":
+				news[i].UpdatedTime, err = time.Parse("RFC3339", val.(string))
+			case "likes":
+				news[i].Likes = val.(string)
+			case "comments":
+				data := val.(map[string]interface{})
+				news[i].Comments = parseComments(data["data"].([]interface{}))
+			}
+		}
+	}
+	return
+}
