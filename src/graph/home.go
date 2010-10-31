001b6b418edb336dcfcba7aec44d38ba1c5e4694
Marcel Hauf <marcel.hauf@googlemail.com>
2010-10-31 21:36:20 +0100

    Extract Paging from home.go to paging.go

--- src/graph/home.go
+++ src/graph/home.go
@@ -35,17 +35,6 @@ func FetchHomeByURL(url string) (home Home, err os.Error) {
 	return
 }
 
-type Paging struct {
-	Previous string
-	Next     string
-}
-
-func parsePaging(value map[string]interface{}) (paging Paging) {
-	paging.Previous = value["previous"].(string)
-	paging.Next = value["next"].(string)
-	return
-}
-
 type News struct {
 	ID          string
 	From        Object
---
+++ src/graph/paging.go
@@ -0,0 +1,12 @@
+package facebook
+
+type Paging struct {
+	Previous string
+	Next     string
+}
+
+func parsePaging(value map[string]interface{}) (paging Paging) {
+	paging.Previous = value["previous"].(string)
+	paging.Next = value["next"].(string)
+	return
+}
