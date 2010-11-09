28b2b86d0f41a8f8b7c474cc742e9dff6314ed44
Marcel Hauf <marcel.hauf@googlemail.com>
2010-11-09 20:07:43 +0100

    Add post.go which contains the Post object.

---
+++ src/graph/post.go
@@ -0,0 +1,58 @@
+package facebook
+
+import (
+	"time"
+)
+
+type Posts struct {
+	// The post ID
+	ID string
+	// An object containing the ID and name of the user who posted the message
+	From Object
+	// A list of the profiles mentioned or targeted in this post
+	To []Object
+	// The message
+	Message string
+	// If available, a link to the picture included with this post
+	Picture Picture
+	// The link attached to this post
+	Link string
+	// The name of the link
+	Name string
+	// The caption of the link (appears beneath the link name)
+	Caption string
+	// A description of the link (appears beneath the link caption)
+	Description string
+	// If available, the source of the stream attachment to add to this post (for example, a flash file or image)
+	Source string
+	// A link to an icon representing the type of this post
+	Icon string
+	// A string indicating which application was used to create this post
+	Attribution string
+	/* A list of available actions on the post (including commenting, liking, and an optional app-specified action),
+	 * encoded as objects with keys for the 'name' and 'link'.
+	 */
+	Actions []Link
+	/* 
+	 * An object that defines the privacy setting for a post, video, or album. It contains the following fields:
+	 * value(string) : The privacy value for the object, specify one of EVERYONE, CUSTOM, ALL_FRIENDS, NETWORKS_FRIENDS, FRIENDS_OF_FRIENDS.
+	 * friends(string) : For CUSTOM settings, this indicates which users can see the object. Can be one of EVERYONE, NETWORKS_FRIENDS (when the object can be seen by networks and friends), FRIENDS_OF_FRIENDS, ALL_FRIENDS, SOME_FRIENDS, SELF, or NO_FRIENDS (when the object can be seen by a network only).
+	 * networks(string) : For CUSTOM settings, specify a comma-separated list of network IDs that can see the object, or 1 for all of a user's networks.
+	 * allow(string) : When friends is set to SOME_FRIENDS, specify a comma-separated list of user IDs and friend list IDs that 'can' see the post.
+	 * deny(string) : When friends is set to SOME_FRIENDS, specify a comma-separated list of user IDs and friend list IDs that 'cannot' see the post. 
+	 * 
+	 * Note: This privacy setting only applies to posts to the current or specified user's own Wall; Facebook ignores this setting for targeted Wall posts (when the user is writing on the Wall of a friend, Page, event, group connected to the user). Consistent with behavior on Facebook, all targeted posts are viewable by anyone who can see the target's Wall.
+	 * Privacy Policy: Any non-default privacy setting must be intentionally chosen by the user. You may not set a custom privacy setting unless the user has proactively specified that they want this non-default setting.
+	 */
+	Privacy Object //?
+	// The number of likes on this post
+	Likes float64
+	// The time the post was initially published
+	CreatedTime *time.Time
+	// The time of the last comment on this post
+	UpdatedTime *time.Time
+}
+
+// All of the comments on this post
+func (p *Post) GetComments() (comments []Comment) {
+}
