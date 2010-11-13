8c263c04eb7da64ab323cfab198193667c319ea0
Marcel Hauf <marcel.hauf@googlemail.com>
2010-11-13 19:35:37 +0100

    Add Venue.

---
+++ src/graph/venue.go
@@ -0,0 +1,20 @@
+package facebook
+
+type Venue struct {
+	Street    string
+	City      string
+	State     string
+	Country   string
+	Latitude  float64
+	Longitude float64
+}
+
+func parseVenue(value map[string]interface{}) (v Venue) {
+	v.Street = value["street"].(string)
+	v.City = value["city"].(string)
+	v.State = value["state"].(string)
+	v.Country = value["country"].(string)
+	v.Latitude = value["latitude"].(float64)
+	v.Longitude = value["longitude"].(float64)
+	return
+}
