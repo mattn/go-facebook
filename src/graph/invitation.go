5c22f1f14629a4e3396833c2c6dd52634ebcd43e
Marcel Hauf <marcel.hauf@googlemail.com>
2010-11-20 18:18:22 +0100

    Change Invitation to the new OO design.

--- src/graph/invitation.go
+++ src/graph/invitation.go
@@ -1,7 +1,7 @@
 package graph
 
 import (
-//	"os"
+	"os"
 )
 
 type Invitation struct {
@@ -10,25 +10,21 @@ type Invitation struct {
 	// rsvp_status, not_replied, attending, unsure or declined
 	RSVPStatus string
 }
-/*
-func GetInvitations(URL string) (invs []Invitation, err os.Error) {
-	// TODO: Check for valid ID
-	b, err := fetchPage(URL)
-	m, err := getJsonMap(b)
-	data, ok := m["data"].([]interface{})
-	if !ok {
-		err = os.NewError("GetInvitations: data could not be found.")
+
+func getInvitations(url string) (invs []Invitation, err os.Error) {
+	data, err := getData(url)
+	if err != nil {
 		return
 	}
 	for i, v := range data {
-		invs[i].parseData(v.(map[string]interface{}))
+		invs[i] = parseInvitation(v.(map[string]interface{}))
 	}
 	return
 }
-*/
-func (i *Invitation) parseData(value map[string]interface{}) {
-	i.Name = value["name"].(string)
-	i.ID = value["id"].(string)
-	i.RSVPStatus = value["rsvp_status"].(string)
+
+func parseInvitation(value map[string]interface{}) (invi Invitation) {
+	invi.Name = value["name"].(string)
+	invi.ID = value["id"].(string)
+	invi.RSVPStatus = value["rsvp_status"].(string)
 	return
 }
