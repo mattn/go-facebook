8b488455779eb37eccc933e28588ad02af5cce2d
Marcel Hauf <marcel.hauf@googlemail.com>
2010-11-21 22:56:33 +0100

    Temp bypass an illegal char at the end of the encoded SignedRequest data part.

--- src/oauth/signedrequest.go
+++ src/oauth/signedrequest.go
@@ -18,11 +18,14 @@ type SignedRequest struct {
 func ParseSignedRequest(input string) (sr SignedRequest, err os.Error) {
 	d := strings.Split(input, ".", -1)
 	//signature := DecodeBase64URL(d[0])
-	data := DecodeBase64URL(d[1])
-
+	data, err := DecodeBase64URL(d[1])
+	if err != nil {
+		return
+	}
+	data = data[:len(data)-1] // TEMP: Bypass an illegal char at the end
 	var values interface{}
 	if err = json.Unmarshal([]byte(data), &values); err != nil {
-		return
+		return sr, os.NewError("Error: ParseSignedRequest: json.Unmarshal: " + err.String() + " in " + data)
 	}
 	val := values.(map[string]interface{})
 	sr.Algorithm = val["algorithm"].(string)
@@ -34,12 +37,12 @@ func ParseSignedRequest(input string) (sr SignedRequest, err os.Error) {
 	// TODO: Check SignedRequest with signature
 }
 
-func DecodeBase64URL(s string) string {
+func DecodeBase64URL(s string) (string, os.Error) {
 	s += "=" // Facebook fix, facebook sends corrupted base64_url
 	dbuf := make([]byte, base64.URLEncoding.DecodedLen(len(s)))
 	_, err := base64.URLEncoding.Decode(dbuf, []byte(s))
 	if err != nil {
-		println(err.String())
+		return "", err
 	}
-	return string(dbuf)
+	return string(dbuf), err
 }
