df98c1056f59aad4c3a7b7426ced7246e83a2da2
Marcel Hauf <marcel.hauf@googlemail.com>
2010-12-20 20:57:19 +0100

    Add parseUnknownObject to get an Go interface of a FB obj.

--- src/graph/tools.go
+++ src/graph/tools.go
@@ -29,6 +29,98 @@ func parseObjects(value []interface{}) (objs []Object) {
 	return
 }
 
+func parseUnknownObject(value map[string]interface{}) (obj interface{}, err os.Error) {
+	t, ok := value["type"].(string)
+	if ok {
+		switch t {
+		case "user":
+			obj, err = parseUser(value)
+			if err == nil {
+				return
+			}
+		case "application":
+			obj, err = parseApplication(value)
+			if err == nil {
+				return
+			}
+		case "page":
+			obj, err = parsePage(value)
+			if err == nil {
+				return
+			}
+		case "group":
+			obj, err = parseGroup(value)
+			if err == nil {
+				return
+			}
+		case "photo":
+			obj, err = parsePhoto(value)
+			if err == nil {
+				return
+			}
+		case "album":
+			obj, err = parseAlbum(value)
+			if err == nil {
+				return
+			}
+		case "checkin":
+			obj, err = parseCheckin(value)
+			if err == nil {
+				return
+			}
+		case "comment":
+			obj, err = parseComment(value)
+			if err == nil {
+				return
+			}
+		case "event":
+			obj, err = parseEvent(value)
+			if err == nil {
+				return
+			}
+		case "friendlist":
+			// TODO
+		case "insights":
+			// TODO
+		case "link":
+			obj, err = parseLink(value)
+			if err == nil {
+				return
+			}
+		case "message":
+			// TODO
+		case "note":
+			obj, err = parseNote(value)
+			if err == nil {
+				return
+			}
+		case "status": //StatusMessage
+			obj, err = parseStatusMessage(value)
+			if err == nil {
+				return
+			}
+		case "subscription":
+			obj, err = parseSubscription(value)
+			if err == nil {
+				return
+			}
+		case "thread":
+			// TODO
+		case "video":
+			obj, err = parseVideo(value)
+			if err == nil {
+				return
+			}
+		case "post":
+			obj, err = parsePost(value)
+			if err == nil {
+				return
+			}
+		}
+	}
+	return obj, os.NewError("No field type detected, can't figure out the object type currently. TODO: Try and error over all possible FB obj types.")
+}
+
 type URL struct {
 	Name string
 	URL  string
